2757853296e50fd6d4ca4294bb38c82fcdcc0a55
bmreddyrs1408 <bmreddyrs1408@gmail.com>
2024-10-26 18:52:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,9 @@ region="us-east-1"
 }
 resource "aws_instance" "one" {
 count = 4
-ami ="ami-06b21ccaeff8cd686"
-key_name="aws-myvtech"
+ami ="ami-0ddc798b3f1a5117e"
+instance_type = "t2.micro"
+key_name="terraform"
 vpc_security_group_ids = ["sg-0c7ff5813c220ff6c"]
 tags = {
 Name = var.instance_names[count.index]
